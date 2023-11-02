2a3830f8b2ab3c9023b5abc1c7897cc8edd6a102
RydKrm <alriyadkarim@gmail.com>
2023-11-02 23:20:50 +0600

    Set up project

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+//import  Gallery  from './components/Gallery';
+function App() {
+
+  return (
+    <>
+    {/* <Gallery/> */}
+    Setup ollyo task app
+    </>
+  )
+}
+
+export default App
